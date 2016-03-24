1aca4dd51868976d6870fbbc03e75fa0cfbcd75d
AceXIE <xk2010@163.com>
2016-03-24 14:04:16 +0800

    整数转为Roman http://www.lintcode.com/en/problem/integer-to-roman/

--- src/main/java/IntegertoRoman.java
+++ src/main/java/IntegertoRoman.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
+
 /**
  * Created by Administrator on 2016/3/18.
  */
 public class IntegertoRoman {
 
-//    public String intToRoman(int n) {
-//
-//    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(intToRoman(scanner.nextInt()));
+    }
+    public static String intToRoman(int n) {
+        String res = new String();
+
+        return res;
+    }
 
 }
