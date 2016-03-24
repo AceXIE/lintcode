803b722076711a33ffaffbe75d449041e7620cc8
xiekai <xk2010@163.com>
2016-03-24 22:53:07 +0800

    http://www.lintcode.com/en/problem/binary-tree-inorder-traversal/

---
+++ src/main/java/BinaryTreeInorderTraversal.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * Created by xk on 2016/3/24.
+ */
+public class BinaryTreeInorderTraversal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+    public ArrayList<Integer> inorderTraversal(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+
+        }
+        return res;
+    }
+}
--- src/main/java/IntegertoRoman.java
+++ src/main/java/IntegertoRoman.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
+
 /**
  * Created by Administrator on 2016/3/18.
  */
 public class IntegertoRoman {
 
-//    public String intToRoman(int n) {
-//
-//    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(intToRoman(scanner.nextInt()));
+    }
+
+    public static String intToRoman(int n) {
+        String res = new String();
+        String[][] strings = new String[][]{
+                {"","I","II","III","IV","V","VI","VII","VIII","IX"},
+                {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"},
+                {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"},
+                {"","","MM",""}
+        };
+        int row=0;
+        while (n > 0) {
+            int col = n%10;
+            res = strings[row][col] + res;
+            n/=10;
+            row++;
+        }
+        return res;
+    }
 
 }
