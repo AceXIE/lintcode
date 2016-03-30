959a06222ceab35222ab2baf4af7ec894fa0a6b4
xiekai <xk2010@163.com>
2016-03-30 22:19:03 +0800

    mogu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,10 +8,37 @@ public class Main {
     public static void main(String args[]) {
         Scanner cin = new Scanner(System.in);
         int a, b;
-        while (cin.hasNextInt()) {
+        while (cin.hasNext()) {
+            int i = 0;
+            boolean flag = false;
             a = cin.nextInt();
             b = cin.nextInt();
-            System.out.println(a + b);
+            for (i = a; i <= b; i++) {
+                if (isShui(i)) {
+                    if (!flag) {
+                        System.out.print(i);
+                    } else {
+                        System.out.print(" ");
+                        System.out.print(i);
+                    }
+                    flag = true;
+                }
+            }
+            if (!flag){
+                System.out.print("no\n");
+            } else {
+                System.out.println();
+            }
         }
+    }
+    public static boolean isShui(int a) {
+        int sum = 0;
+        int origin = a;
+        while (a > 0) {
+            sum += (int)Math.pow(a%10, 3);
+            a /= 10;
+        }
+        if (sum == origin) return true;
+        else return false;
     }
 }
---
+++ src/main/java/Main2.java
@@ -0,0 +1,5 @@
+/**
+ * Created by xk on 2016/3/30.
+ */
+public class Main2 {
+}
