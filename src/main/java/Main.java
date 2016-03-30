2469fe1de2f587b055330a95bb9d2b090418675f
xiekai <xk2010@163.com>
2016-03-30 21:00:10 +0800

    增加Main程序，For 蘑菇街

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,47 +1,17 @@
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Scanner;
-import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
 /**
- * Created by Administrator on 2016/3/19.
- * 华为实习机试体验1
+ * Created by xk on 2016/3/30.
+ * 蘑菇街
  */
 public class Main {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Scanner cin = new Scanner(System.in);
-        String add = "addCandidate";
-        String vote = "vote";
-        List<String> name = new ArrayList<String>();
-        List<String> cixu = new ArrayList<String>();
-        int wuxiao = 0;
-        HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
-        int idex = 0;
-        String oror = new String();
-        while ( !((oror = cin.next()).equals( "getVoteResult"))) {
-
-            if (oror.equals(add)) {
-                String addname = cin.next();
-                if (!hashMap.containsKey(addname)) {
-                    cixu.add(addname);
-                    idex++;
-                    hashMap.put(addname, 0);
-                }
-            } else if (oror.equals(vote)){
-                String addname = cin.next();
-                if (hashMap.containsKey(addname)) {
-                    int cishu = hashMap.get(addname);
-                    cishu++;
-                    hashMap.put(addname, cishu);
-                } else {
-                    wuxiao++;
-                }
-            }
+        int a, b;
+        while (cin.hasNextInt()) {
+            a = cin.nextInt();
+            b = cin.nextInt();
+            System.out.println(a + b);
         }
-        for (int i = 0; i < hashMap.size(); i++){
-            System.out.print(cixu.get(i) + " ");
-            System.out.println(hashMap.get(cixu.get(i)));
-        }
-        System.out.println(wuxiao);
     }
 }
