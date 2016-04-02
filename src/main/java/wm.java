911fd81c2ea9f4da36a89970992e01b69531c024
xiekai <xk2010@163.com>
2016-04-02 23:10:55 +0800

    腾讯笔试大题

--- src/main/java/wm.java
+++ src/main/java/wm.java
@@ -1,28 +1,28 @@
-import java.util.logging.Handler;
-
-/**
- * Created by xk on 2016/4/2.
- */
-public class wm {
-    public abstract class SyncTask<R> {
-        protected abstract R run();
-        private R result;
-        private byte[] lock = new byte[0];
-        private boolean notified = false;
-        private Runnable task = new Runnable() {
-            @Override
-            public void run() {
-                R ret = SyncTask.this.run();
-                synchronized (lock) {
-                    result = ret;
-                    lock.notify();
-                    notified = true;
-                }
-            }
-        };
-        public R start(final long timeout, final R defaultRet, Handler handler) {
-
-        }
-    }
-
-}
+//import java.util.logging.Handler;
+//
+///**
+// * Created by xk on 2016/4/2.
+// */
+//public class wm {
+//    public abstract class SyncTask<R> {
+//        protected abstract R run();
+//        private R result;
+//        private byte[] lock = new byte[0];
+//        private boolean notified = false;
+//        private Runnable task = new Runnable() {
+//            @Override
+//            public void run() {
+//                R ret = SyncTask.this.run();
+//                synchronized (lock) {
+//                    result = ret;
+//                    lock.notify();
+//                    notified = true;
+//                }
+//            }
+//        };
+//        public R start(final long timeout, final R defaultRet, Handler handler) {
+//
+//        }
+//    }
+//
+//}
