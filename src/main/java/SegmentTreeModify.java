37bdcc9796896bdc941a0314b512ae701c7e1f94
xiekai <xk2010@163.com>
2016-04-03 16:04:32 +0800

    线段树的修改 http://www.lintcode.com/en/problem/segment-tree-modify/

--- src/main/java/SegmentTreeModify.java
+++ src/main/java/SegmentTreeModify.java
@@ -2,5 +2,22 @@
  * Created by xk on 2016/4/3.
  */
 public class SegmentTreeModify {
+    public void modify(SegmentTreeNode root, int index, int value) {
+        if (root == null) return;
+        if (root.start > index || root.end < index) {
+            return;
+        }
+        if (root.start == index && root.end == index) {
+            root.max = value;
+            return;
+        }
+        int mid = (root.start + root.end)/2;
+        if (index <= mid) {
+            modify(root.left, index, value);
+        } else {
+            modify(root.right, index, value);
+        }
+        root.max = Math.max(root.left.max, root.right.max);
 
+    }
 }
