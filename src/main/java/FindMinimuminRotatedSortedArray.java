a9184692e365fd9ae5cd625d5ac6b41677c3f636
AceXIE <xk2010@163.com>
2016-04-06 21:14:38 +0800

    http://www.lintcode.com/en/problem/find-minimum-in-rotated-sorted-array/

---
+++ src/main/java/FindMinimuminRotatedSortedArray.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Administrator on 2016/4/6.
+ */
+public class FindMinimuminRotatedSortedArray {
+    public static void main(String[] args) {
+        int[] num = {4, 5, 6, 7, 0, 1, 2};
+        System.out.println(findMin(num));
+    }
+    public static int findMin(int[] num) {
+        int l = 0, r = num.length -1;
+        if (num.length == 1 || num[l] < num[r]) return num[0];
+        while (l + 1 != r) {
+            int mid = (l + r)/2;
+            if (num[mid] > num[l]) {
+                l = mid;
+            }
+            if (num[mid] < num[r]){
+                r = mid;
+            }
+        }
+        return num[r];
+    }
+}
