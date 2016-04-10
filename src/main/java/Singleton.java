a97085f500b2fb1745b0d21918766f942332516b
AceXIE <1048345175@qq.com>
2016-04-10 21:07:00 +0800

    单例模式 http://www.lintcode.com/en/problem/singleton/

---
+++ src/main/java/Singleton.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2016/4/10.
+ */
+public class Singleton {
+    private static Singleton instance = null;
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
