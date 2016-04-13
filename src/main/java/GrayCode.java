157bf94e5810c36858938ec16b5472eb8bca41d0
AceXIE <1048345175@qq.com>
2016-04-13 23:44:33 +0800

    http://www.lintcode.com/en/problem/gray-code/

---
+++ src/main/java/GrayCode.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+/**
+ * Created by Administrator on 2016/4/13.
+ */
+public class GrayCode {
+    public static void main(String[] args) {
+        int n = 2;
+//        System.out.print()
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        res = grayCode(n);
+        for (int i = 0; i < res.length; i++) {
+            System.out.print(res.);
+        }
+    }
+    public static ArrayList<Integer> grayCode(int n) {
+
+    }
+}
