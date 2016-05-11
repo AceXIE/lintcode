efba9c759ebca67c92e998ba1fc618290be1cc25
AceXIE <1048345175@qq.com>
2016-05-11 23:40:47 +0800

    http://www.lintcode.com/en/problem/majority-number-ii/#

--- src/main/java/MajorityNumberII.java
+++ src/main/java/MajorityNumberII.java
@@ -12,8 +12,13 @@ public class MajorityNumberII {
         for (int i = 0; i < a.length; i++) {
             nums.add(a[i]);
         }
-        System.out.print(majorityNumber(nums));
+        System.out.print(majorityNumber2(nums));
     }
+    /***
+     * 采用Map进行计数，空间复杂度较高
+     * @param nums
+     * @return
+     */
     public static int majorityNumber(ArrayList<Integer> nums) {
         if (nums == null || nums.size() == 0) return -1;
         Map<Integer,Integer> map = new LinkedHashMap<Integer, Integer>();
@@ -29,5 +34,29 @@ public class MajorityNumberII {
             }
         }
         return -1;
+    }
+    public static int majorityNumber2(ArrayList<Integer> nums) {
+        if (nums == null || nums.size() == 0) return -1;
+        int n1 = 0, n2 = 0, count1 = 0, count2 = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (count1 == 0) {
+                n1 = nums.get(i);
+            }
+            if (count2 == 0 && n1 != nums.get(i)) {
+                n2 = nums.get(i);
+            }
+            if (n1 != nums.get(i) && n2 != nums.get(i)
+                    && count1 != 0 && count2 != 0) {
+                count1--;
+                count2--;
+            }
+            if (n1 == nums.get(i)) {
+                count1++;
+            }
+            if (n2 == nums.get(i)) {
+                count2++;
+            }
+        }
+        return (count1>count2?n1:n2);
     }
 }
