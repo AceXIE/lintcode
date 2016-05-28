b731daa35ec8b854ec467b83df0b6a937b1114f8
AceXIE <1048345175@qq.com>
2016-05-28 23:53:54 +0800

    http://www.lintcode.com/en/problem/convert-sorted-list-to-balanced-bst/

---
+++ src/main/java/ConvertSortedListtoBalancedBST.java
@@ -0,0 +1,56 @@
+/**
+ * Created by Administrator on 2016/5/28.
+ */
+public class ConvertSortedListtoBalancedBST {
+    public static void main(String[] args) {
+        ListNode root = new ListNode(1);
+        ListNode p = root;
+        for (int i = 2; i < 4; i++) {
+            ListNode listNode = new ListNode(i);
+            p.next = listNode;
+            p = p.next;
+        }
+//        sortedListToBST(root);
+        printTree(sortedListToBST(root));
+    }
+    static ListNode currentHead = null;
+    TreeNode buildTree(int start, int end) {
+        if(start>end) {
+            return null;
+        }
+        int mid = start + (end - start)/2;
+        TreeNode left = buildTree(start, mid-1);
+        TreeNode root = new TreeNode(currentHead.val);
+        root.left = left;
+        currentHead = currentHead.next;
+        root.right = buildTree(mid + 1, end);
+        return root;
+    }
+    public static TreeNode sortedListToBST(ListNode head) {
+        int len = 0;
+        if (head == null) return null;
+        ListNode ph = head;
+        while (ph != null) {
+            len++;
+            ph = ph.next;
+        }
+        return helper(head, 0, len - 1);
+    }
+    public static TreeNode helper(ListNode head, int start, int end) {
+        if (start > end) return null;
+        int mid = start + (end - start)/2;
+        TreeNode leftTree = helper(head, start, mid - 1);
+        TreeNode root = new TreeNode(head.val);
+        root.left = leftTree;
+        head = head.next;
+        root.right = helper(head, mid + 1, end);
+        return root;
+    }
+    public static void printTree(TreeNode root) {
+        if (root != null) {
+            System.out.print(root.val);
+            printTree(root.left);
+            printTree(root.right);
+        }
+    }
+}
