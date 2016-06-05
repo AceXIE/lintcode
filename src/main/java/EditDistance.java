c484349d37eb5bdeb7961697d0098bf148201ee4
AceXIE <1048345175@qq.com>
2016-06-05 22:43:26 +0800

    DP问题 http://www.lintcode.com/en/problem/edit-distance/

--- src/main/java/EditDistance.java
+++ src/main/java/EditDistance.java
@@ -5,9 +5,29 @@ public class EditDistance {
     public static void main (String[] args) {
         String word1 = "mart";
         String word2 = "karma";
-//        System.out.print(minDistance(word1, word2));
+        System.out.print(minDistance(word1, word2));
     }
-//    public static int minDistance(String word1, String word2) {
-//
-//    }
+    public static int minDistance(String word1, String word2) {
+        if (word1.length() == 0) return word2.length();
+        if (word2.length() == 0) return word1.length();
+
+        int [][] dp = new int[word1.length() + 1][word2.length() + 1];
+        for (int i = 0; i <= word1.length(); i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 0; i <= word2.length(); i++) {
+            dp[0][i] = i;
+        }
+
+        for (int i = 1; i <= word1.length() ; i++) {
+            for (int j = 1; j <= word2.length(); j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j - 1], dp[i - 1][j]), dp[i][j - 1]) + 1;
+                }
+            }
+        }
+        return dp[word1.length()][word2.length()];
+    }
 }
