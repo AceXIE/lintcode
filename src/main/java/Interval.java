d8042f43668687fd8840206df84fbf56eaf6cd13
AceXIE <1048345175@qq.com>
2016-07-09 12:06:06 +0800

    线段树 http://www.lintcode.com/en/problem/interval-sum/

---
+++ src/main/java/Interval.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2016/7/9.
+ */
+public class Interval {
+    int start;
+    int end;
+    Interval(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+}
---
+++ src/main/java/IntervalSum.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Administrator on 2016/7/9.
+ */
+public class IntervalSum {
+//    public static void main(String[] args) {
+//        int[] A = {1,2,7,8,5};
+//        ArrayList<Interval> queries = new ArrayList<Interval>();
+//        Interval interval0 = new Interval(0,4);
+//        Interval interval1 = new Interval(1,2);
+//        Interval interval2 = new Interval(2,4);
+//        queries.add(interval0);queries.add(interval1);queries.add(interval2);
+//        ArrayList<Long> longArrayList = intervalSum(A, queries);
+//        System.out.print(longArrayList);
+//    }
+//    public static ArrayList<Long> intervalSum(int[] A, ArrayList<Interval> queries) {
+//
+//    }
+}
