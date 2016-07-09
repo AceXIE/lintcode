a3b3ad35344a502b50323b6bc94bf9331a01addc
AceXIE <1048345175@qq.com>
2016-07-09 15:13:27 +0800

    http://www.lintcode.com/en/problem/longest-palindromic-substring/

---
+++ src/main/java/LongestPalindromicSubstring.java
@@ -0,0 +1,27 @@
+/**
+ * Created by Administrator on 2016/7/9.
+ */
+public class LongestPalindromicSubstring {
+    public static void main(String[] args) {
+        String s = "abcdzdcab";
+        System.out.print(longestPalindrome(s));
+    }
+    public static String longestPalindrome(String s) {
+        if (s == null || s.length() == 0) return "";
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        String ans = "";
+        int max = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            for (int j = i; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j) && (j - i <=2 || dp[i+1][j-1])) {
+                    dp[i][j] = true;
+                    if (max<j -i +1) {
+                        max = j -i + 1;
+                        ans = s.substring(i, j+1);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
